7a0ffc907bdeb2f10e671beb0b7cb4442362353c
JP <jp@angelle.me>
2024-02-14 10:02:20 -0600

    fix: latest eth buy endpoint

--- src/getLatestETHBuy.ts
+++ src/getLatestETHBuy.ts
@@ -9,12 +9,17 @@ const ethereumAccountId = process.env.ETHEREUM_ACCOUNT_ID;
 export const getLatestETHBuy = async () => {
   const payload = constructPayload({
     method: "GET",
-    path: `/v2/accounts/${ethereumAccountId}/transactions?status=completed&type=buy`,
+    path: `/v2/accounts/${ethereumAccountId}/transactions`,
   });
 
   const {
-    data: { data: completedBuyTransactions },
+    data: { data: transactions },
   } = await axios(payload);
+
+  const completedBuyTransactions = transactions.filter(
+    (transaction: { type: "buy"; status: "completed" }) =>
+      transaction.type === "buy" && transaction.status === "completed"
+  );
 
   return completedBuyTransactions[0];
 };
